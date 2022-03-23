2d23f734bcaaa7e1a31f4bf0d2b6b8bc46a5010e
Raphael Marques <raphaelmarques77@gmail.com>
2022-03-23 21:04:36 +0000

    Update AWS S3 configuration

--- config/env/production/plugin.js
+++ config/env/production/plugin.js
@@ -1,12 +1,14 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: "aws-s3",
-    providerOptions: {
-      accessKeyId: env("AWS_ACCESS_KEY_ID"),
-      secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
-      region: env("AWS_S3_BUCKET_REGION"),
-      params: {
-        Bucket: env("AWS_S3_BUCKET_NAME"),
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
+        },
       },
     },
   }
