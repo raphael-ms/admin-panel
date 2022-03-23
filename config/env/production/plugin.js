00de4aa48eb6b9c386e55db605dcdcef0f7b647b
Raphael Marques <raphaelmarques77@gmail.com>
2022-03-23 22:17:37 +0000

    remove middleware config

--- config/env/production/plugin.js
+++ config/env/production/plugin.js
@@ -1,14 +1,12 @@
 module.exports = ({ env }) => ({
   upload: {
-    config: {
-      provider: 'aws-s3',
-      providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: env('AWS_REGION'),
-        params: {
-          Bucket: env('AWS_BUCKET'),
-        },
+    provider: "aws-s3",
+    providerOptions: {
+      accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
+      region: env("AWS_S3_BUCKET_REGION"),
+      params: {
+        Bucket: env("AWS_S3_BUCKET_NAME"),
       },
     },
   }
