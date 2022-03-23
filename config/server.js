88942fc46290b05a51bda2f52b135973f0fb08ad
Raphael Marques <raphaelmarques77@gmail.com>
2022-03-23 17:43:42 +0000

    Update production configuration

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 4200),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '93434575f92445145dd51085089c337d'),
